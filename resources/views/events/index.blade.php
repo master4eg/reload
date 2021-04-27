b5387cddd25691853c9be9ffe64a9c908e458db1
master4eg <stadnyuk_vitaly@mail.ru>
2021-04-27 13:10:30 +0500

    try ajax fix

--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -1,6 +1,12 @@
 @extends('layout.main')
 @section('title', 'Планировщик записей')
 
+@push('scripts')
+    <script src="https://cdn.jsdelivr.net/npm/fullcalendar@5.6.0/main.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/fullcalendar-scheduler@5.6.0/locales-all.min.js"></script>
+    <script src="{{ asset('js/events.js') }}"></script>
+@endpush
+
 @section('content')
     <div class="card">
         <div class="card-header">Календарь</div>
@@ -8,12 +14,9 @@
             <div id="calendar"></div>
         </div>
     </div>
+    @extends('events.modals.create')
+    @extends('events.modals.edit')
+    @extends('events.modals.error')
 @endsection
-@extends('events.modals.create')
-@extends('events.modals.edit')
-@extends('events.modals.error')
-@push('scripts')
-    <script src="https://cdn.jsdelivr.net/npm/fullcalendar@5.6.0/main.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/fullcalendar-scheduler@5.6.0/locales-all.min.js"></script>
-    <script src="{{ asset('js/events.js') }}"></script>
-@endpush
+
+
